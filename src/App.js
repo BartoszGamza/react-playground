fd4e448a872f0e15b1682c53a8af17e0f20b26a5
bartoszgamza <bartosz.gamza12@gmail.com>
2018-07-26 23:29:55 +0200

    setState added

--- src/App.js
+++ src/App.js
@@ -10,13 +10,23 @@ class App extends Component {
     ]
   }
 
-  render() {
+  switchName = () => {
+    this.setState({
+      persons: [
+        { name: 'Rafal', age: 21} ,
+        { name: 'Filop', age: 72}
+      ]
+    })
+    console.log('names changed')
+  }
+
+  render () {
     return (
       <div className="App">
         <h1>hello</h1>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobby is reading</Person>
-        <button>Switch name</button>
+        <button onClick={this.switchName}>Switch name</button>
       </div>
     );
   }
