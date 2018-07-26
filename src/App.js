34635368d3efc5d6047aca8090f45a2f77aee58f
bartoszgamza <bartosz.gamza12@gmail.com>
2018-07-27 00:02:17 +0200

    added two types of styling

--- src/App.js
+++ src/App.js
@@ -10,30 +10,53 @@ class App extends Component {
     ]
   }
 
-  switchName = () => {
+  switchName = (newName) => {
     this.setState({
       persons: [
-        { name: 'Rafal', age: 21} ,
+        { name: newName, age: 21} ,
         { name: 'Filop', age: 72}
       ]
     })
     console.log('names changed')
   }
 
+  changeName = (event) => {
+    this.setState({
+      persons: [
+        { name: 'Bart', age: 21} ,
+        { name: event.target.value, age: 72}
+      ]
+    })
+  }
+
   render () {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>hello</h1>
         <Person
           name={this.state.persons[0].name}
-          age={this.state.persons[0].age} />
+          age={this.state.persons[0].age}
+          input={this.changeName}/>
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
-          click={this.switchName}>
+          click={this.switchName.bind(this, 'Max!')} //bind is better option than below
+          input={this.changeName}>
           My Hobby is reading
         </Person>
-        <button onClick={this.switchName}>Switch name</button>
+        <button
+          style={style}
+          onClick={() => this.switchName('Maximillian!!')}>
+          Switch name
+        </button>
       </div>
     );
   }
