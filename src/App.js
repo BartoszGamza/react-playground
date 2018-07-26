7e994d83f6cbd4e376fe0fb3af4bcc28bcd8fb94
bartoszgamza <bartosz.gamza12@gmail.com>
2018-07-26 23:04:08 +0200

    added state

--- src/App.js
+++ src/App.js
@@ -3,11 +3,20 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Bart', age: 21} ,
+      { name: 'Filop', age: 22}
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>hello</h1>
-        <Person name="Bart" age="21" />
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobby is reading</Person>
+        <button>Switch name</button>
       </div>
     );
   }
